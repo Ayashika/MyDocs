ec57c876ea46cf56b22b5f14ad1ec927a428f20f
Ayashika <97466562+Ayashika@users.noreply.github.com>
2023-06-13 15:18:40 +0530

    Secured db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose  from 'mongoose';
 
 const Connection = async () => {
-    const URL = `mongodb://ayashika2624:Loveugod@ac-kkpnu51-shard-00-00.8gb4qxn.mongodb.net:27017,MyDocs-shard-00-01.8gb4qxn.mongodb.net:27017,ac-kkpnu51-shard-00-02.8gb4qxn.mongodb.net:27017/?ssl=true&replicaSet=atlas-sxplpr-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const URL = `mongodb://ayashika2624:<password>@ac-kkpnu51-shard-00-00.8gb4qxn.mongodb.net:27017,MyDocs-shard-00-01.8gb4qxn.mongodb.net:27017,ac-kkpnu51-shard-00-02.8gb4qxn.mongodb.net:27017/?ssl=true&replicaSet=atlas-sxplpr-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
